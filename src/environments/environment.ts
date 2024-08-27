22a8ac0f2b3aa9015bf1fe93ed4075c39530cd34
K-Saeed <62437371+K-Saeed@users.noreply.github.com>
2024-08-27 20:33:33 +0300

    update and finish requests

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   //  apiUrl: 'http://localhost:8080'
   // apiUrl:'https://meter-backend.onrender.com'
   apiUrl: 'http://3.83.97.229:8080'
-  // apiUrl: 'http://44.202.8.76:8080/'
+  // apiUrl: 'http://44.202.8.76:8080'
 };
