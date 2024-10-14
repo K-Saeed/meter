c7604ba5441dcf9b1ff07113f75a051af5c5983c
K-Saeed <62437371+K-Saeed@users.noreply.github.com>
2024-10-14 22:46:51 +0300

    handle open files and download it

--- src/app/core/components/service-requests/request-show-modal/request-show-modal.component.ts
+++ src/app/core/components/service-requests/request-show-modal/request-show-modal.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { RequestResponseDto } from '../models/request-table.model';
-import { HttpClient } from '@angular/common/http';
+import { Component, Input } from "@angular/core";
+import { RequestResponseDto } from "../models/request-table.model";
+import { HttpClient } from "@angular/common/http";
+import { saveAs } from "file-saver";
 
 @Component({
-  selector: 'app-request-show-modal',
-  templateUrl: './request-show-modal.component.html',
-  styleUrls: ['./request-show-modal.component.css']
+  selector: "app-request-show-modal",
+  templateUrl: "./request-show-modal.component.html",
+  styleUrls: ["./request-show-modal.component.css"],
 })
 export class RequestShowModalComponent {
-  @Input () request?: RequestResponseDto;
+  @Input() request?: RequestResponseDto;
 
-  activeLink: string = 'details';
-  constructor(private http: HttpClient) { }
+  activeLink: string = "details";
+  constructor(private http: HttpClient) {}
 
   setActiveLink(link: string, event: Event) {
     event.preventDefault();
@@ -19,16 +20,13 @@ export class RequestShowModalComponent {
   }
 
   downloadFile(file: any): void {
-    // Fetch the file as a Blob
-    this.http.get(file.filePath, { responseType: 'blob' }).subscribe(blob => {
-      // Create a URL for the Blob object
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-
-      link.download = file.fileName || 'download';
-      link.click();
-      window.URL.revokeObjectURL(url);
-    });
+    const link = document.createElement("a");
+    link.href = file.filePath;
+    link.target = '_blank'; // Opens the link in a new tab
+    link.download = file.filename || 'default-filename'; // Optionally suggest a filename for download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
+
 }
