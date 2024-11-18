7c04b63829f7c6214f502cf4cf89c2592895c7c1
K-Saeed <62437371+K-Saeed@users.noreply.github.com>
2024-11-18 12:10:42 +0200

    handle search in porposalas table by request id

--- src/app/core/components/providers-proposals/proposal-action/proposal-action.component.ts
+++ src/app/core/components/providers-proposals/proposal-action/proposal-action.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-proposal-action',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./proposal-action.component.css']
 })
 export class ProposalActionComponent {
+  searchTerm: string = '';
 
+  @Output() searchChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  applySearch() {
+    this.searchChanged.emit(this.searchTerm);
+  }
 }
