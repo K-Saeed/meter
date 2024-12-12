d479a7f7f993ca01204a2b145d1e14fbe40d9b00
K-Saeed <62437371+K-Saeed@users.noreply.github.com>
2024-12-12 13:51:32 +0300

    handle open submitting work in pop up

--- src/app/core/components/work-submission/work-submission-attachment-modal/work-submission-attachment-modal.component.ts
+++ src/app/core/components/work-submission/work-submission-attachment-modal/work-submission-attachment-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-work-submission-attachment-modal',
@@ -7,5 +8,34 @@ import { Component, Input } from '@angular/core';
 })
 export class WorkSubmissionAttachmentModalComponent {
   @Input() submission: any; // Input to receive submission details
+  showFilePopup: boolean = false;
+  selectedFileUrl: SafeResourceUrl | null = null;
+  isImageFile: boolean = false;
+  constructor(
+    private sanitizer: DomSanitizer
+    ) {}
 
+  downloadFile(file: any): void {
+    const link = document.createElement("a");
+    link.href = file.filePath;
+    link.target = "_blank";
+    link.download = file.filename || "default-filename";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  closePopup() {
+    this.showFilePopup = false;
+    this.selectedFileUrl = null;
+  }
+
+  openFileInPopup(file: any) {
+    const filePath = file.filePath;
+    this.isImageFile = /\.(png|jpg|jpeg)$/.test(filePath);
+    this.selectedFileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      filePath + (this.isImageFile ? "" : "#toolbar=0")
+    );
+    this.showFilePopup = true;
+  }
 }
