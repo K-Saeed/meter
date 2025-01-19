195903297fea1a6acbead4e93eb28c51aecb432f
amrmostafa007 <amr.mostafa.abdelkader@gmail.com>
2025-01-20 00:51:41 +0200

    save latest state in checking permissions

--- src/app/shared/service/auth/auth-guard.service.ts
+++ src/app/shared/service/auth/auth-guard.service.ts
@@ -6,6 +6,26 @@ import { AuthService } from "./auth.service";
   providedIn: "root"
 })
 export class AuthGuardService implements CanActivate {
+
+  private routePermissionMap: { [key: string]: string } = {
+    dashboard: 'dashboard',
+    servicerequests: 'requests',
+    users: 'users',
+    providersproposal: 'proposals',
+    conversations: 'conversations',
+    transactions: 'transactions',
+    notifications: 'notifications',
+    employees: 'employees',
+    roles: 'employees',//
+    settings: 'dashboard',//
+    products: 'products',
+    worksubmission: 'work_submission',
+    WorkSubmission: 'work_submission',
+    chat: 'chat',
+    unfinisheduser: 'users',//
+  };
+  
+
   constructor(
     private router: Router,
     private authService: AuthService
@@ -13,7 +33,17 @@ export class AuthGuardService implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (this.authService.isAuthenticated()) {
-      return true;
+
+      const userPermissions = JSON.parse(localStorage.getItem('permissions') || '{}');
+      const currentRoute = route.routeConfig?.path;
+  
+      const permissionKey = this.routePermissionMap[currentRoute || ''];
+  
+      if (permissionKey && userPermissions[permissionKey]?.includes('READ')) {
+        return true;
+      }
+      this.router.navigate(["/signin"]);
+      return false;
     } else {
       this.router.navigate(["/signin"]); // Redirect to sign-in page if not authenticated
       return false;
