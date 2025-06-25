811b5225998c90dc590b921608b6ea739a66456f
Eman Elshawardy <63519201+EmanElshawardy@users.noreply.github.com>
2025-06-25 19:43:49 +0300

    done title, action components in notification

--- src/app/core/components/notifications/notifications-action/notifications-action.component.ts
+++ src/app/core/components/notifications/notifications-action/notifications-action.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-notifications-action',
   templateUrl: './notifications-action.component.html',
   styleUrls: ['./notifications-action.component.css']
 })
-export class NotificationsActionComponent {
+export class NotificationsActionComponent implements OnInit {
+  searchTerm: string = '';
+  currentLang: string = 'en';
 
+  @Output() searchChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(private translateService: TranslateService) {
+    this.currentLang = this.translateService.currentLang;
+  }
+
+  ngOnInit(): void {
+    this.translateService.onLangChange.subscribe(lang => {
+      this.currentLang = lang.lang;
+    });
+  }
+
+  applySearch() {
+    this.searchChanged.emit(this.searchTerm);
+  }
 }
--- src/app/core/components/notifications/notifications-title/notifications-title.component.ts
+++ src/app/core/components/notifications/notifications-title/notifications-title.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { TranslationService } from 'src/app/shared/service/translation.service';
 @Component({
   selector: 'app-notifications-title',
   templateUrl: './notifications-title.component.html',
   styleUrls: ['./notifications-title.component.css']
 })
-export class NotificationsTitleComponent {
+export class NotificationsTitleComponent implements OnInit {
+  currentLang = 'en';
 
+  constructor(
+    public translationService: TranslationService,
+    private translate: TranslateService
+  ) { }
+  ngOnInit(): void {
+    this.currentLang = this.translate.currentLang;
+
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLang = event.lang;
+    });
+  }
+
+  get currentFlag(): string {
+    return this.currentLang === 'ar' ? 'ar.png' : 'en.png';
+  }
+
+  get currentLabel(): string {
+    return this.currentLang === 'ar' ? 'Arabic' : 'English';
+  }
 }
