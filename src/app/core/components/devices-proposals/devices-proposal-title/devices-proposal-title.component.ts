ce29a0126654a1c0746a618b9345359c3c79c47e
Eman Elshawardy <63519201+EmanElshawardy@users.noreply.github.com>
2025-06-28 16:21:11 +0300

    done devices proposal

--- src/app/core/components/devices-proposals/devices-proposal-title/devices-proposal-title.component.ts
+++ src/app/core/components/devices-proposals/devices-proposal-title/devices-proposal-title.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { TranslationService } from 'src/app/shared/service/translation.service';
 
 @Component({
   selector: 'app-devices-proposal-title',
   templateUrl: './devices-proposal-title.component.html',
   styleUrls: ['./devices-proposal-title.component.css']
 })
-export class DevicesProposalTitleComponent {
+export class DevicesProposalTitleComponent implements OnInit {
+  currentLang = 'en';
 
+  constructor(
+    public translationService: TranslationService,
+    private translate: TranslateService
+  ) { }
+  ngOnInit(): void {
+    this.currentLang = this.translate.currentLang;
+
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLang = event.lang;
+    });
+  }
 }
