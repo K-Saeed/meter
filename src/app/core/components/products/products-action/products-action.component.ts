a51a540b7736c0e2aa9502d7e020ba7bbd16ce12
Eman Elshawardy <63519201+EmanElshawardy@users.noreply.github.com>
2025-06-28 17:09:35 +0300

    done products page

--- src/app/core/components/products/products-action/products-action.component.ts
+++ src/app/core/components/products/products-action/products-action.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-products-action',
   templateUrl: './products-action.component.html',
   styleUrls: ['./products-action.component.css']
 })
-export class ProductsActionComponent {
+export class ProductsActionComponent implements OnInit {
+  searchTerm: string = '';
+  currentLang: string = 'en';
 
+  @Output() searchChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(private translateService: TranslateService) {
+    this.currentLang = this.translateService.currentLang;
+  }
+
+  ngOnInit(): void {
+    this.translateService.onLangChange.subscribe(lang => {
+      this.currentLang = lang.lang;
+    });
+  }
+
+  applySearch() {
+    this.searchChanged.emit(this.searchTerm);
+  }
 }
